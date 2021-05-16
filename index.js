5b8f8f2fe03ba4ef424b950d056cefd8365e3a7b
Thibault Milan <thibault.milan@smile.fr>
2021-05-16 17:07:26 +0200

    Rendering the table with points

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
+// Initialisation de variables et d'objets par default
+const defaultScore = [{
+        id: "g1",
+        name: "Les Langues Européenes",
+        points: 0
+    },
+    {
+        id: "g2",
+        name: "La géographie",
+        points: 0
+    },
+    {
+        id: "g3",
+        name: "Les Monnaies",
+        points: 0
+    },
+    {
+        id: "g4",
+        name: "La géographie",
+        points: 0
+    }
+];
+Handlebars.registerHelper("total", function(value) {
+    let count = 0;
+    value.forEach((item, i) => {
+        count += item.points;
+    });
+    console.log("score totalisé : %s pts", count);
+    return count + "pts";
+});
+
+// On init le localStorage
+if (localStorage.getItem('score') == null) {
+    console.info('Le localStorage a été initialisé');
+    localStorage.setItem('score', JSON.stringify(defaultScore));
+}
+// On charge le tableau des scores dans la mémoire
+var scores = JSON.parse(localStorage.getItem('score'));
+
+
+function renderScore() {
+    var source = document.getElementById('score-tpl').innerHTML;
+    var template = Handlebars.compile(source);
+    var rendered = template({
+        'score': scores
+    });
+    document.getElementById('score').innerHTML = rendered;
+}
+
+// On attends que la page soit chargée
 window.addEventListener('load', function() {
+    // On affiche les scores
+    renderScore();
 
     QrScanner.WORKER_PATH = "node_modules/qr-scanner/qr-scanner-worker.min.js";
     // on localise l'element video qui va servir à donner le feedback client
@@ -16,4 +68,5 @@ window.addEventListener('load', function() {
 
     // on démarre le scan
     qrScanner.start();
+
 }, false);
